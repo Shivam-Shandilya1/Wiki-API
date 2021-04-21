7011c5eca9f64cb62ee2e471dd83d559b3645e01
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-04-21 14:53:13 +0530

    set up the server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const mongoose = require("mongoose");
+
+const app=express();
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
+
+const articleSchema = new mongoose.Schema({
+    title:String,
+    content:String
+
+});
+
+const Article = mongoose.model("Article","articleSchema");
+const port = process.env.PORT || 3000;
+app.listen(port);
