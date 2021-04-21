97223435672be3e251a3997f9b1a2dc278cfd203
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-04-21 18:41:19 +0530

    server setup

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
+app.use(express.json());
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true,useUnifiedTopology: true});
 
 const articleSchema = new mongoose.Schema({
     title:String,
@@ -17,6 +17,20 @@ const articleSchema = new mongoose.Schema({
 
 });
 
-const Article = mongoose.model("Article","articleSchema");
-const port = process.env.PORT || 3000;
-app.listen(port);
+const Article = mongoose.model("Article",articleSchema);
+
+app.get("/articles",function(req,res)
+{
+    Article.find(function(err,foundArticles)
+    {
+            res.send(foundArticles);
+        
+        
+        
+    });
+});
+
+app.listen(3000,function()
+{
+    console.log("Server started at port 3000.");
+});
